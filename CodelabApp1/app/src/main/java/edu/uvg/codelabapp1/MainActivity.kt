ed59210809f4c9e9f0675db12a26a4309da2d5ca
Moises Alonso <maalonso@uvg.edu.gt>
2024-08-13 18:14:56 -0600

    Columns modified by me

--- CodelabApp1/app/src/main/java/edu/uvg/codelabapp1/MainActivity.kt
+++ CodelabApp1/app/src/main/java/edu/uvg/codelabapp1/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -29,27 +31,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
-    Surface (
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
+fun MyApp(
+        modifier: Modifier = Modifier,
+        names: List<String> = listOf("World","Compose")
     ){
-        Greeting("Android")
-    }
+    Column (modifier){
+        for (name in names){
+            Greeting(name = name)
+        }
+    }    
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.fillMaxWidth().padding(5.dp),
+
+
+    ) {
+        Column(modifier = modifier.padding(24.dp)) {
+            Text(text = "Hello")
+            Text(text = name)
+        }
     }
 
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     CodelabApp1Theme {
