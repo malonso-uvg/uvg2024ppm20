c3b9f46f7b36fefe6124b7103c12d563432f175f
Moises Alonso <maalonso@uvg.edu.gt>
2024-08-13 18:03:22 -0600

    Basic Composable, Until Reusing composables

---
+++ CodelabApp1/app/src/main/java/edu/uvg/codelabapp1/MainActivity.kt
@@ -0,0 +1,58 @@
+package edu.uvg.codelabapp1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import edu.uvg.codelabapp1.ui.theme.CodelabApp1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CodelabApp1Theme {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier){
+    Surface (
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ){
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CodelabApp1Theme {
+        MyApp()
+    }
+}
