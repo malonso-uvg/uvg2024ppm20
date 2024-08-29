1e08c22c898fb618cecdc980a587f360b436152c
Moises Alonso <maalonso@uvg.edu.gt>
2024-08-29 00:38:13 -0600

    04 Android Navigation Using Drawer complete example

--- 04_AndroidNavigationUsingDrawer/app/src/main/java/edu/uvg/navigationdrawer/MainActivity.kt
+++ 04_AndroidNavigationUsingDrawer/app/src/main/java/edu/uvg/navigationdrawer/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,7 +21,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationDrawerTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                    , color = MaterialTheme.colorScheme.background
+                ) {
+                    MainNavigation()
+                }
             }
         }
     }
