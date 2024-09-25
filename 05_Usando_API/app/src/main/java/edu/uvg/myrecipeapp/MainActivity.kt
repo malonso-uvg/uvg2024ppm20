fa6fc0f7520d6626857ddde6ca659bc3e2b5f619
Moises Alonso <maalonso@uvg.edu.gt>
2024-09-24 18:21:33 -0600

    Ejemplo uso de API

---
+++ 05_Usando_API/app/src/main/java/edu/uvg/myrecipeapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package edu.uvg.myrecipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import edu.uvg.myrecipeapp.ui.theme.MyRecipeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyRecipeAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RecipeScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
